28062e0879dc46aab1bd2dd680d424d9c5a18c47
dustin <dustin@pop-os.localdomain>
2019-03-29 14:40:15 -0400

    Made substring.pl more universal

--- substring.pl
+++ substring.pl
@@ -1,14 +1,32 @@
 #!/usr/bin/perl -w
 
-$filename = 'Danio_rerio.Zv9.dna.chromosome.1.fa';
+### writen by Dustin Wcisel on 3/29/2019
+### this script takes in a fasta file containing only one sequence and returns a fasta file
+### containing sequence of defined coordinates
+### use: perl substring.pl Danio_rerio.GRCz10.dna.chromosome.1.fa 56968494 56968513
 
-open(CHR1, $filename);
+my $filename = $ARGV[0] or die "use: substring.pl sequence.fasta LEFT_COORD RIGHT_COORD\n";
+my $sub_left = $ARGV[1];
+my $sub_right = $ARGV[2];
+my $base_pairs = $sub_right-$sub_left;
 
-@chr1 = <CHR1>;
-close CHR1;
-chomp @chr1;
+$sub_left = $sub_left-1; #perl starts at 0
 
-$chr1 = join('', @chr1);
+open(DNA, $filename);
 
-print ">chr1_58415260-58415760\n", substr($chr1, 58415260, 500), "\n";
+while (<DNA>) { #this loop is necessary to remove any all blank lines, newlines, and the fasta header
+	next if ($_ =~ /^>/); #ignores the line with the sequence name
+	next if ($_ =~ /^\s*$/ ); #ignores any all blank lines
+	chomp;
+	push @dna, $_;
+}
 
+my $dna = join ('', @dna);
+
+my $substring = substr($dna, $sub_left, $base_pairs);
+
+print ">substring\n";
+
+for $print_format ($substring =~ m/.{1,60}/gs) {
+	print $print_format, "\n";
+}
